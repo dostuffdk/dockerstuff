72cda7793351c018e3d3f9571730aeed24150cfe
Anders Mogensen <anders@dostuff.dk>
2017-02-11 17:37:43 +0100

    Tmp test stuff

--- website/www/index.php
+++ website/www/index.php
@@ -3,6 +3,16 @@ $hostname = "mysql";
 $username = "root";
 $password = "1234";
  
+
+if(function_exists('mcrypt_encrypt')) {
+    echo "mcrypt is loaded!";
+} else {
+    echo "mcrypt isn't loaded!";
+}
+
+echo json_encode(array('one' => 1));
+var_dump(json_decode('{"one":1}', true));
+
 try {
     $dbh = new PDO("mysql:host=$hostname;dbname=mysql", $username, $password);
     echo "Connected to database"; // check for connection

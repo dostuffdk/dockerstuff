24b84191380924f9a85202b4f27e455c7b55d04f
Anders Mogensen <anders@dostuff.dk>
2017-06-10 12:01:35 +0200

    Run the stylelint with webpack to lint for css errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var StyleLintPlugin = require("stylelint-webpack-plugin");
 
 module.exports = {
 	entry: [
@@ -9,7 +10,12 @@ module.exports = {
 		filename: 'app.js'
 	},
 	plugins: [
-		new ExtractTextPlugin("./../css/style.css", {allChunks: false}) // we need to step back so we dont put css files in the js dir
+		new ExtractTextPlugin("./../css/style.css", {allChunks: false}), // we need to step back so we dont put css files in the js dir
+		new StyleLintPlugin({
+			configFile: './.stylelintrc',
+			context: './assets/css/',
+			files: '**/*.css'
+		})
 	],
 	module: {
 		preLoaders: [

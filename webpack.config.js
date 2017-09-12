44a8d2c4f4dd8351c0c8d9b230d107d58cf325f1
Anders Mogensen <anders@dostuff.dk>
2017-09-12 22:28:31 +0200

    Use extracttextplugin to extract styles to actual css file instead of having it reside inside the bundled js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
 	entry: './assets/app.js',
@@ -20,11 +21,14 @@ module.exports = {
 			},
 			{
 				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader'
-				]
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: 'css-loader'
+				})
 			}
 		]
-	}
+	},
+	plugins: [
+		new ExtractTextPlugin('./../css/style.css')
+	]
 };

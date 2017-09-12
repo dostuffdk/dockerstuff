937f48f384c17144347470ecf7323226f3de1173
Anders Mogensen <anders@dostuff.dk>
2017-09-12 22:08:20 +0200

    ES2015 template literal test to see if Babel works

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
 const salaries = [10000, 20000, 30000];
 const with1000Increment = salaries.map(s => s + 1000);
 console.log(with1000Increment);
+
+const bar = 'baz';
+console.log(`Foo bar: ${baz}`);

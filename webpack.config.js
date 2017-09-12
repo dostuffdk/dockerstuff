d04d3f001f04494d9275757ce1e0958cfc3b15d5
Anders Mogensen <anders@dostuff.dk>
2017-09-12 22:09:01 +0200

    Starting over configuring with webpack3 syntax. Staring with babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,23 @@
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var StyleLintPlugin = require("stylelint-webpack-plugin");
+const path = require('path');
 
 module.exports = {
-	entry: [
-		'./assets/app.js'
-	],
+	entry: './assets/app.js',
 	output: {
-		path: './website/js', // set the output for all handled files, also css
-		filename: 'app.js'
+		filename: 'app.js',
+		path: path.resolve('./website/js')
 	},
-	plugins: [
-		new ExtractTextPlugin("./../css/style.css", {allChunks: false}), // we need to step back so we dont put css files in the js dir
-		new StyleLintPlugin({
-			configFile: './.stylelintrc',
-			context: './assets/',
-			files: ['**/*.css', '**/*.vue']
-		})
-	],
 	module: {
-		preLoaders: [
-			{
-				test: /\.(js|vue)/,
-				loader: 'eslint',
-				exclude: /node_modules/
-			}
-		],
-		loaders: [
+		rules: [
 			{
 				test: /\.js$/,
-				loader: 'babel',
-				exclude: /node_modules/,
-				query: {
-					plugins: ['transform-runtime']
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env']
+					}
 				}
-			},
-			{
-				test: /\.css$/,
-				loader: ExtractTextPlugin.extract('style-loader', 'css-loader?importLoaders=1!postcss-loader')
 			}
 		]
-	},
-	postcss: function() {
-		return [
-			require('postcss-import'),
-			require('postcss-cssnext')
-		];
-	},
-	eslint: {
-		failOnWarning: false,
-		failOnError: true
 	}
 };

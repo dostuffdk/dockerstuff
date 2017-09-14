4ef216eeb5c97089968602d9f87ae20a5f78dc4d
Anders Mogensen <anders@dostuff.dk>
2017-09-14 21:28:44 +0200

    Postcss with cssnext

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 	entry: './assets/app.js',
@@ -23,7 +23,15 @@ module.exports = {
 				test: /\.css$/,
 				use: ExtractTextPlugin.extract({
 					fallback: 'style-loader',
-					use: 'css-loader'
+					use: [
+						{
+							loader: 'css-loader',
+							options: {
+								importLoaders: 1
+							}
+						},
+						'postcss-loader'
+					]
 				})
 			}
 		]
